fdeb69c22ea844b862604ca161423d56f4c9823b
Lavínia <lavizsenai@gmail.com>
2025-05-16 06:38:47 -0300

    cantina 10.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Cantina_10._0_Projeto_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            carrinhoListBox = new ListBox();
+            finalizarButton = new PictureBox();
+            excluirItemButton = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)finalizarButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)excluirItemButton).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-435, -20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1344, 65);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click_1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(7, 7);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(246, 31);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(66, 7);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(197, 31);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.FromArgb(230, 255, 0);
+            pictureBox6.Cursor = Cursors.Hand;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(766, 8);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(22, 29);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(230, 255, 0);
+            pictureBox4.Cursor = Cursors.Hand;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(725, 10);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(25, 25);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(623, -30);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(186, 546);
+            pictureBox5.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            pictureBox5.Visible = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(202, 196, 183);
+            pictureBox7.Cursor = Cursors.Hand;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(649, 10);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(18, 18);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 7;
+            pictureBox7.TabStop = false;
+            pictureBox7.Visible = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // carrinhoListBox
+            // 
+            carrinhoListBox.BackColor = Color.FromArgb(202, 196, 183);
+            carrinhoListBox.FormattingEnabled = true;
+            carrinhoListBox.ItemHeight = 15;
+            carrinhoListBox.Location = new Point(637, 45);
+            carrinhoListBox.Name = "carrinhoListBox";
+            carrinhoListBox.Size = new Size(151, 364);
+            carrinhoListBox.TabIndex = 8;
+            carrinhoListBox.Visible = false;
+            // 
+            // finalizarButton
+            // 
+            finalizarButton.Image = (Image)resources.GetObject("finalizarButton.Image");
+            finalizarButton.Location = new Point(712, 394);
+            finalizarButton.Name = "finalizarButton";
+            finalizarButton.Size = new Size(76, 68);
+            finalizarButton.SizeMode = PictureBoxSizeMode.CenterImage;
+            finalizarButton.TabIndex = 9;
+            finalizarButton.TabStop = false;
+            finalizarButton.Visible = false;
+            // 
+            // excluirItemButton
+            // 
+            excluirItemButton.Image = (Image)resources.GetObject("excluirItemButton.Image");
+            excluirItemButton.Location = new Point(634, 394);
+            excluirItemButton.Name = "excluirItemButton";
+            excluirItemButton.Size = new Size(76, 68);
+            excluirItemButton.SizeMode = PictureBoxSizeMode.CenterImage;
+            excluirItemButton.TabIndex = 10;
+            excluirItemButton.TabStop = false;
+            excluirItemButton.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(excluirItemButton);
+            Controls.Add(finalizarButton);
+            Controls.Add(carrinhoListBox);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox7);
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "Cantina 10.0";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)finalizarButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)excluirItemButton).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox7;
+        private ListBox carrinhoListBox;
+        private PictureBox finalizarButton;
+        private PictureBox excluirItemButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+namespace Cantina_10._0_Projeto_Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Visible = true;
+            pictureBox7.Visible = true;
+            carrinhoListBox.Visible = true;
+            finalizarButton.Visible = true;
+            excluirItemButton.Visible = true;
+            pictureBox5.BringToFront();
+            pictureBox7.BringToFront();
+            finalizarButton.BringToFront();
+            excluirItemButton.BringToFront();
+            carrinhoListBox.BringToFront();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Visible = false;
+            pictureBox7.Visible = false;
+            finalizarButton.Visible = false;
+            excluirItemButton.Visible = false;
+            carrinhoListBox.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
